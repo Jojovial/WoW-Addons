8011e00601b7332f1c738a6314b6f98c6f7d52d6
JoJo <joehearthstone@gmail.com>
2023-11-17 12:02:14 -0500

    hello world works in game!

--- HelloWorld/HelloWorld.lua
+++ HelloWorld/HelloWorld.lua
@@ -1,1 +1,16 @@
-message("Hello, World!")
+
+SLASH_HELLO1 = "/helloworld"
+SLASH_HELLO2 = "/hellow"
+
+
+-- /helloworld Paul
+local function HelloWorldHandler(name)
+
+    -- name === paul
+    -- Hello, Jasdavi!
+    message("Hello, " .. name .. "!")
+end
+
+SlashCmdList["HELLO"] = HelloWorldHandler
+
+-- local name = UnitName("player")
