c50105d2f8c5af735716bdea61574fbbf38abb48
JoJo <joehearthstone@gmail.com>
2023-11-17 12:22:10 -0500

    first done woo

--- HelloWorld/HelloWorld.lua
+++ HelloWorld/HelloWorld.lua
@@ -3,12 +3,26 @@ SLASH_HELLO1 = "/helloworld"
 SLASH_HELLO2 = "/hello"
 
 
+local function showGreeting(name)
+    local greeting = "Hello, " .. name .. "!"
+
+    message(greeting)
+end
+
+
 -- /helloworld Paul
 local function HelloWorldHandler(name)
+    local userAddedName = string.len(name) > 0
 
     -- name === paul
-    -- Hello, Jasdavi!
-    message("Hello, " .. name .. "!")
+
+    if(userAddedName) then
+        showGreeting(name)
+    else
+        local playerName = UnitName("player")
+
+        showGreeting(playerName)
+    end
 end
 
 SlashCmdList["HELLO"] = HelloWorldHandler
