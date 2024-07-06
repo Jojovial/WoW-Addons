30e0681e58161b4d647c74dc5422ed1b5acad86d
JoJo <joehearthstone@gmail.com>
2024-07-06 16:46:57 -0400

    refactor

--- HelloWorld/HelloWorld.lua
+++ HelloWorld/HelloWorld.lua
@@ -1,30 +1,23 @@
-
 SLASH_HELLO1 = "/helloworld"
 SLASH_HELLO2 = "/hello"
 
-
+-- Function to display a greeting message
 local function showGreeting(name)
     local greeting = "Hello, " .. name .. "!"
-
     message(greeting)
 end
 
-
--- /helloworld Paul
+-- Handler function for the slash commands
 local function HelloWorldHandler(name)
-    local userAddedName = string.len(name) > 0
-
-    -- name === paul
-
-    if(userAddedName) then
+    -- Check if a name was provided
+    if string.len(name) > 0 then
         showGreeting(name)
     else
+        -- If no name is provided, use the player's name
         local playerName = UnitName("player")
-
         showGreeting(playerName)
     end
 end
 
+-- Register the handler for the slash commands
 SlashCmdList["HELLO"] = HelloWorldHandler
-
--- local name = UnitName("player")
