1917a5cdac5282bb1a131108bea441a02837251e
JoJo <joehearthstone@gmail.com>
2024-07-06 16:47:58 -0400

    refactored

--- MusicPlayer/MusicPlayer.lua
+++ MusicPlayer/MusicPlayer.lua
@@ -1,9 +1,11 @@
+-- Define sound types
 local soundType = {
     SOUND = 1,
     GAME_MUSIC = 2,
     CUSTOM = 3
 }
 
+-- Define the sounds with their properties
 local sounds = {
     ["murloc"] = {
         ["sound"] = 416,
@@ -32,63 +34,60 @@ local sounds = {
     }
 }
 
-
-
+-- Define slash commands
 SLASH_SOUND1 = "/playsound"
 SLASH_STOPSOUND1 = "/stopsound"
 
+-- Function to display the list of available sounds
 local function displaySoundList()
     print("--------------------")
-    for command in pairs(sounds) do
-        local description = sounds[command].description
-        print("Command: /playsound " .. command .. " - Description: " .. description)
+    for command, details in pairs(sounds) do
+        print("Command: /playsound " .. command .. " - Description: " .. details.description)
     end
     print("--------------------")
 end
 
 local customSoundId
 
+-- Function to play a track based on its type
 local function playTrack(track)
-  print(track.description)
+    print(track.description)
 
-  if(track.type == soundType.GAME_MUSIC) then
-      PlayMusic(track.sound)
-
-      print("To stop the music type /stopsound")
-  elseif(track.type == soundType.SOUND) then
-      PlaySound(track.sound)
-  elseif(track.type == soundType.CUSTOM) then
-      stopSoundHandler()
-      customSoundId = select(2, PlaySoundFile(track.sound))
-  end
+    if track.type == soundType.GAME_MUSIC then
+        PlayMusic(track.sound)
+        print("To stop the music type /stopsound")
+    elseif track.type == soundType.SOUND then
+        PlaySound(track.sound)
+    elseif track.type == soundType.CUSTOM then
+        stopSoundHandler()
+        customSoundId = select(2, PlaySoundFile(track.sound))
+    end
 end
 
+-- Function to handle the /playsound command
 local function playSoundHandler(trackId)
-    if(string.len(trackId) > 0) then
-        local matchesKnownTrack = sounds[trackId] ~= nil
-
-        if (matchesKnownTrack) then
-            local track = sounds[trackId]
-
+    if string.len(trackId) > 0 then
+        local track = sounds[trackId]
+        if track then
             playTrack(track)
         else
             displaySoundList()
-
             print(trackId .. " - Doesn't match a known track.")
         end
     else
         displaySoundList()
     end
-  end
+end
 
+-- Function to stop currently playing sounds
 local function stopSoundHandler()
     StopMusic()
-
-    if(customSoundId ~= nil) then
+    if customSoundId then
         StopSound(customSoundId)
         customSoundId = nil
     end
 end
 
+-- Register the slash command handlers
 SlashCmdList["SOUND"] = playSoundHandler
 SlashCmdList["STOPSOUND"] = stopSoundHandler
